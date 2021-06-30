f548ddf010328499c1b479e6a6c5c3383d443025
Shan Rauf <s.rauf124@gmail.com>
2021-06-29 18:11:37 -0700

    Add setup script

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # dotfiles
+
+# Setup
+
+1. Setup WSL
+2. Clone https://github.com/shanrauf/dotfiles and move files into home directory
+3. sudo ./setup.sh
---
+++ setup.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env
+
+sudo apt-get update
+
+sudo apt-get install wget
+
+# Download my dotfiles
+git clone https://github.com/shanrauf/dotfiles
+cp ~/dotfiles/. ~
+
+# Oh My Zsh
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+## Plugins (already specified in .zshrc)
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# Powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+# Restart shell
+echo "Installation completed - restart shell to see changes"
