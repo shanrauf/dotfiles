efcc5c87667b8329e23b94089e8977c660fcf248
Shan Rauf <s.rauf124@gmail.com>
2021-08-11 10:26:05 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -18,5 +18,8 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 # Powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
+# Cleaning up
+rm -rf dotfiles
+
 # Restart shell
 echo "Installation completed. Restart shell to see changes"
